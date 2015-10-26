9f517174d4c82e504e85845a2d195a3a5afbb24e
Wojciech Siewierski <wojciech.siewierski@onet.pl>
2015-10-26 23:28:09 +0100

    Initial commit

---
+++ apt-ldd.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+
+# apt-ldd.sh --- Get the packages containing the missing shared libraries
+
+# Copyright (C) 2015 Wojciech Siewierski <wojciech dot siewierski at onet dot pl>
+
+# Author: Wojciech Siewierski <wojciech dot siewierski at onet dot pl>
+
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 3
+# of the License, or (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+usage()
+{
+    cat <<EOF
+Usage:
+    $ $0 -h
+    $ ldd <file> | $0 [-a <architecture>]
+    $ $0 -f <file> [-a <architecture>]
+EOF
+}
+
+if [ -t 0 ]; then
+    usage
+    exit
+fi
+
+ARCH=$(dpkg --print-architecture)
+
+while getopts "a:f:h" ARG; do
+    case "$ARG" in
+        a)
+            ARCH=$OPTARG
+            ;;
+        f)
+            FILE=$OPTARG
+            ;;
+        h)
+            usage
+            exit
+            ;;
+        ?)
+        ;;
+    esac
+done
+shift $((OPTIND-1))
+
+if [ -n "$FILE" ]; then
+    ldd "$FILE"
+else
+    cat
+fi |
+    awk '/not found/ { print $1 }' |
+    parallel -j10 'apt-file search {} | head -n1' |
+    uniq |
+    perl -pe '
+BEGIN { $arch = shift }
+s/:.*$/:$arch/
+' -- "$ARCH"
